804f9830378d655f1af758db03c2f951184dfe67
Stefano Lupo <lupos@tcd.ie>
2019-03-10 23:25:35 +0000

    Running for each object type now too

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import typing
+from typing import List, Dict
 import json
 import os
 from pprint import pprint
@@ -9,7 +9,7 @@ from collections import defaultdict
 
 from metric_name import Metric
 
-Metrics = typing.List[Metric]
+Metrics = List[Metric]
 
 topology = "linear-three"
 deadReckoning = False
@@ -22,27 +22,33 @@ with open(os.path.join(directory, histogramFile)) as f:
     histograms = json.load(f)
 
 metrics : Metrics = [Metric(name, values) for name, values in histograms.items()]
-metrics : Metrics = [m for m in metrics if m.objectType == 'status']
 
-metricsByType = defaultdict(list)
+MetricsByObjectType = Dict[str, Dict[str, Metrics]]
+metricsByObjectType = {"status": {}, "block": {}, "projectile": {}}
+
 for playerMetric in metrics:
-    metricsByType[playerMetric.metricType].append(playerMetric)
 
-print(metricsByType)
+    metricsForObjectType = metricsByObjectType[playerMetric.objectType]
+    if playerMetric.metricType in metricsForObjectType:
+        metricsForObjectType[playerMetric.metricType].append(playerMetric)
+    else:
+        metricsForObjectType[playerMetric.metricType] = [playerMetric]
 
-f, (ax1, ax2) = plt.subplots(1, 2)
-f.suptitle("Player Status Sync")
-# for metricType, metricsForType in metricsByType.items():
+print(metricsByObjectType)
 
-rttMetrics = metricsByType["rtt"]
-for playerMetric in rttMetrics:
-    ax1.hist(playerMetric.histogramValues, label=playerMetric.playerName)
-ax1.set_title("Round Trip Times")
-ax1.legend()
 
-latencyMetrics = metricsByType["latency"]
-for playerMetric in latencyMetrics:
-    ax2.hist(playerMetric.histogramValues, label=playerMetric.playerName)
-ax2.set_title("Latency")
-ax2.legend()
-plt.show()
+for objectType, metricsByMetricType in metricsByObjectType.items():
+    f, (ax1, ax2) = plt.subplots(1, 2)
+    f.suptitle(objectType)
+    rttMetrics = metricsByMetricType["rtt"]
+    for playerMetric in rttMetrics:
+        ax1.hist(playerMetric.histogramValues, label=playerMetric.playerName)
+    ax1.set_title("Round Trip Times")
+    ax1.legend()
+
+    latencyMetrics = metricsByMetricType["latency"]
+    for playerMetric in latencyMetrics:
+        ax2.hist(playerMetric.histogramValues, label=playerMetric.playerName)
+    ax2.set_title("Latency")
+    ax2.legend()
+    plt.show()
